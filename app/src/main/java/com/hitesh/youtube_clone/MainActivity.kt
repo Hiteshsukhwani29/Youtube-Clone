b9b57db45aba0d49466dcc67fcc0f72ff0ec4b3e
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-02-21 15:43:11 +0530

    create fragments

--- app/src/main/java/com/hitesh/youtube_clone/MainActivity.kt
+++ app/src/main/java/com/hitesh/youtube_clone/MainActivity.kt
@@ -2,10 +2,42 @@ package com.hitesh.youtube_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hitesh.youtube_clone.fragment.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottmNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.title =""
+
+        bottmNavigationView = findViewById(R.id.bottom_nav)
+
+        setCurrentFragment(Home())
+
+        bottmNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(Home())
+                R.id.explore -> setCurrentFragment(Explore())
+                R.id.subscription -> setCurrentFragment(Subscriptions())
+                R.id.notification -> setCurrentFragment(Notifications())
+                R.id.library -> setCurrentFragment(Library())
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container1, fragment)
+            commit()
+        }
 }
---
+++ app/src/main/java/com/hitesh/youtube_clone/fragment/Library.kt
@@ -0,0 +1,21 @@
+package com.hitesh.youtube_clone.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hitesh.youtube_clone.R
+
+class Library : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_library, container, false)
+        
+        return v
+    }
+
+}
